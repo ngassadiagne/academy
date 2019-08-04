5c79c0053e9e9c67b20da3a8bb8c0491e8ed0f76
Ngassa Diagne <ngassadiagne@gmail.com>
2019-08-04 21:00:20 +0000

    first commit

--- app web/assets/js/main.js
+++ app web/assets/js/main.js
@@ -1,7 +1,62 @@
 listFilieres = [];
 listClasses = [];
-var select_filiere = document.getElementById('select_filiere');
-var select_classe = document.getElementById('select_classe');
+var select_filiere = document.getElementById('select_filiere'),
+  select_classe = document.getElementById('select_classe'),
+  submitButton = document.getElementById('submitButton'),
+  typeActionRadio = document.form.typeAction,
+  input_prenom = document.getElementById('prenom'),
+  input_nom = document.getElementById('nom'),
+  select_sexe = document.getElementById('sexe'),
+  input_matricule = document.getElementById('matricule'),
+  input_datenaissance = document.getElementById('datenaissance'),
+  input_lieunaissance = document.getElementById('lieunaissance'),
+  input_email = document.getElementById('email'),
+  input_telephone = document.getElementById('telephone'),
+  input_adresse = document.getElementById('adresse'),
+  input_anneeacademique = document.getElementById('anneeacademique'),
+  input_dateinscription = document.getElementById('dateinscription'),
+  input_montantinscription = document.getElementById('montantinscription'),
+  input_mensualite = document.getElementById('mensualite'),
+  input_total = document.getElementById('total');
+
+// on ecoute le changement de type d'action (nouveau ou ancien)
+var prev = null;
+for (var i = 0; i < typeActionRadio.length; i++) {
+  typeActionRadio[i].addEventListener('change', function() {
+    if (this !== prev) {
+      prev = this;
+    }
+    // si l'option ancien est choisi on disable le formulaire
+    if (this.value == 'ancien') {
+      actionAncienFormulaire();
+    }
+
+    // disable du formulaire
+    function actionAncienFormulaire() {
+      input_prenom.setAttribute('disabled', 'disabled');
+      input_nom.setAttribute('disabled', 'disabled');
+      select_sexe.setAttribute('disabled', 'disabled');
+      input_datenaissance.setAttribute('disabled', 'disabled');
+      input_lieunaissance.setAttribute('disabled', 'disabled');
+      input_email.setAttribute('disabled', 'disabled');
+      input_telephone.setAttribute('disabled', 'disabled');
+      input_adresse.setAttribute('disabled', 'disabled');
+      input_matricule.value = '';
+      input_matricule.removeAttribute('disabled');
+      // disable form inscription
+      select_filiere.setAttribute('disabled', 'disabled');
+      select_classe.setAttribute('disabled', 'disabled');
+      input_montantinscription.setAttribute('disabled', 'disabled');
+      input_mensualite.setAttribute('disabled', 'disabled');
+      input_total.setAttribute('disabled', 'disabled');
+      input_dateinscription.setAttribute('disabled', 'disabled');
+      input_anneeacademique.setAttribute('disabled', 'disabled');
+    }
+
+    function actionNouveauFormulaire() {}
+  });
+}
+
 $(document).ready(function() {
   initialisation();
 });
@@ -10,19 +65,7 @@ function initialisation() {
   input_matricule = document.getElementById('matricule');
   input_matricule.value = randomUniqueMatricule();
   initData();
-  select_filiere.addEventListener('change', function() {
-    var filiere = select_filiere.value;
-    listClassesSelectedFiliere = [];
-    listClasses.forEach(classe => {
-      if (classe.filiere == filiere) {
-        listClassesSelectedFiliere.push(classe);
-      }
-    });
-
-    listClassesSelectedFiliere.forEach(function(classe) {
-      console.log(classe);
-    });
-  });
+  events();
 }
 
 function randomUniqueMatricule() {
@@ -42,14 +85,75 @@ function initData() {
   $.get('http://localhost:5000/filieres', function(data) {
     listFilieres = data.filieres;
     // initialisation de la liste des filières
-    listFilieres.forEach(option => {
+    listFilieres.forEach(function(filiere) {
       var elementOption = document.createElement('option');
-      elementOption.textContent = option.libelle;
-      elementOption.value = option.id;
+      elementOption.textContent = filiere.libelle;
+      elementOption.value = filiere.id;
       select_filiere.appendChild(elementOption);
     });
   });
   $.get('http://localhost:5000/classes', function(data) {
     listClasses = data.classes;
+  });
+}
+
+function events() {
+  // ecoute de l'evenement de changement du select filiere
+  select_filiere.addEventListener('change', function() {
+    var filiere = select_filiere.value;
+    listClassesSelectedFiliere = [];
+    listClasses.forEach(function(classe) {
+      if (classe.filiere == filiere) {
+        listClassesSelectedFiliere.push(classe);
+      }
+    });
+    // vider la liste des options de classe
+    $('#select_classe').empty();
+    // on efface contenu des montants
+    input_montantinscription.value = '';
+    input_mensualite.value = '';
+    input_total.value = '';
+    var optionDefault = document.createElement('option');
+    optionDefault.textContent = 'Choisir une classe';
+    optionDefault.value = '';
+    select_classe.append(optionDefault);
+    listClassesSelectedFiliere.forEach(function(classe) {
+      var elementOption = document.createElement('option');
+      elementOption.textContent = classe.libelle;
+      elementOption.value = classe.id;
+      select_classe.appendChild(elementOption);
+    });
+  });
+
+  // ecoute de l'evenement de changement du select classe
+  select_classe.addEventListener('change', function() {
+    var idClasse = select_classe.value;
+    var classe;
+    listClasses.forEach(function(_classe) {
+      if (_classe.id == idClasse) {
+        classe = _classe;
+      }
+    });
+    input_montantinscription.value = classe.montantinscription;
+    input_mensualite.value = classe.mensualite;
+    input_total.value = classe.mensualite + classe.montantinscription;
+  });
+
+  // ecoute de l'evenement submit du formulaire
+  submitButton.addEventListener('click', function() {
+    var prenom = input_prenom.value,
+      nom = input_nom.value,
+      matricule = input_matricule.value,
+      sexe = select_sexe.value,
+      datenaissance = input_datenaissance.value,
+      lieunaissance = input_lieunaissance.value,
+      email = input_email.value,
+      telephone = input_telephone.value,
+      adresse = input_adresse.value,
+      classe = select_classe.value,
+      anneeacademique = input_anneeacademique.value,
+      dateinscription = input_dateinscription.value;
+
+    console.log('ajout');
   });
 }
